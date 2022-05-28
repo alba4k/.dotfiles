addc09632f96e806953467eae0c0e351d852e0f4
alba4k <blaskoazzolaaaron@gmail.com>
2022-05-28 10:14:04 +0200

    committed automatically from update.sh

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,13 +5,12 @@ alias c="clear;fish;exit"
 
 alias ls="exa"
 alias la="exa -a"
-alias ll="exa -al"
-alias t=time
+alias ll="exa -la"
 
 set fish_greeting
-
 export MANPAGER="sh -c 'col -bx | bat -l man -p'"
 export EDITOR=/usr/bin/nvim
 
 albafetch -l arch
 #uptime-record -s
+
