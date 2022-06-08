e17f1cb252c59fbda55140a3668a054c78666774
alba4k <blaskoazzolaaaron@gmail.com>
2022-06-08 22:34:29 +0200

    committed automatically from update.sh

--- .screenlayout/3MCasa.sh
+++ .screenlayout/3MCasa.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 xrandr --output eDP-1 --mode 1920x1080 --pos 0x0 --output DP-1-1 --mode 1920x1080 --pos 3840x0 --output DP-1-3 --primary --mode 1920x1080 --pos 1920x0
 
-~/.fehbg
+#~/.fehbg
+nitrogen --restore &
 
 setxkbmap -layout ch,de
--- usr/bin/autostart.sh
+++ usr/bin/autostart.sh
@@ -5,6 +5,4 @@
 #/usr/bin/powersave
 #/usr/bin/performance
 
-sysctl dev.i915.perf_stream_paranoid=0
-
 HOME=/home/alba4k uptime-record -b
