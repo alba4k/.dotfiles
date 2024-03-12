2ae7e842324c9b7974727511eb2cb734aba62bbf
alba4k <blaskoazzolaaaron@gmail.com>
2024-03-12 07:25:34 +0100

    committed automatically from update.sh

--- .config/hypr/scripts/hyprlock/music-uptime.sh
+++ .config/hypr/scripts/hyprlock/music-uptime.sh
@@ -6,6 +6,6 @@ if grep "Playing" <<< "$playerctl" >/dev/null; then
     playerctl -p "spotify,*" metadata --format "󰎆  {{title}} - {{artist}}"
 else
     echo -n "󱎫  "
-    uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users.*//; s/days/giorni/; s/day/giorno/; s/min/min./; s/([[:digit:]]+):0?([[:digit:]]+)/\1 ore, \2 min./;'
+    uptime | sed -E 's/^[^,]*up *//; s/, *[[:digit:]]* users?.*//; s/days/giorni/; s/day/giorno/; s/min/min./; s/([[:digit:]]+):0?([[:digit:]]+)/\1 ore, \2 min./;'
 fi
 
