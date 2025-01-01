3aeafc2f450e40a93e248808371f1ef50ce00762
alba4k <blaskoazzolaaaron@gmail.com>
2025-01-01 22:52:11 +0100

    added alacritty config

--- userhome/.config/nvim/init.lua
+++ userhome/.config/nvim/init.lua
@@ -110,3 +110,9 @@ vim.cmd.colorscheme "catppuccin"
 
 vim.cmd.colorscheme "catppuccin"
 
+vim.api.nvim_create_autocmd("ExitPre", {
+	group = vim.api.nvim_create_augroup("Exit", { clear = true }),
+	command = "set guicursor=a:ver90",
+	desc = "Set cursor back to beam when leaving Neovim."
+})
+
